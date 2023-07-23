9d11ba6d2e5ffaeda70efadc7db89f801a1b8b35
jyee4187 <70191541+jyee4187@users.noreply.github.com>
2023-07-23 15:56:26 +1200

    laser base bog fix

---
+++ kiwiJam2023/Assets/Scripts/ActivatedLaserBase.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+
+public class ActivatedLaserBase : MonoBehaviour
+{
+    public float spawnInterval = 5f; // Time in seconds before the next laser spawns
+    public float despawnDelay = 2f; // Time in seconds before the laser disappears
+
+    public int damageAmount = 1; // The amount of damage the laser does to the player
+
+    public Sprite activatedBase;
+    public Sprite deactivatedBase;
+    public GameObject laserBase; 
+    public GameObject gameManager;
+    private SpriteRenderer spriteRenderer;
+    private BoxCollider2D boxCollider;
+
+    private void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        boxCollider = GetComponent<BoxCollider2D>();
+
+        InvokeRepeating("SpawnLaser", 0f, spawnInterval);
+    }
+
+    private void SpawnLaser()
+    {
+        spriteRenderer.enabled = true;
+        boxCollider.enabled = true;
+        laserBase.GetComponent<SpriteRenderer>().sprite = activatedBase;
+
+        Invoke("DespawnLaser", despawnDelay);
+    }
+
+    private void DespawnLaser()
+    {
+        spriteRenderer.enabled = false;
+        boxCollider.enabled = false;
+        laserBase.GetComponent<SpriteRenderer>().sprite = deactivatedBase;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            gameManager.GetComponent<gameManager>().onDeath();
+        }
+    }
+}
+
+
